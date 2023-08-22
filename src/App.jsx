26ea37fe00d30daa41364b4e843c6a22586ee1a4
Fatroo <danielsdelfin@gmail.com>
2023-08-22 09:17:25 -0300

    Iniciando o Boyler-Plate Vite+React.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+export default function App() {
+  return (
+  <>
+    <h1>Firts DEPLOY in to VERCEL</h1>
+    <h2>Vite + React</h2>
+    <p>Code By - Rm552184</p>
+    
+
+
+
+
+
+  </>
+
+
+
+
+
+
+
+  )
+}
